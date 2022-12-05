442f8a2a0c3e463f3a464653767a4fe75bbb27fa
George-Marchment <georgemarchment@yahoo.fr>
2022-12-05 08:38:33 +0100

    change again

--- run.sh
+++ run.sh
@@ -11,15 +11,4 @@ nextflow main.nf    \
     --mapping true \
     --indexBam true \
     --countingReads true \
-    --differentialAnalysis true \
-
-    --SRAID ["SRR628589", "SRR628588", "SRR628587", "SRR628586", "SRR628585", "SRR628584", "SRR628583", "SRR628582"] \
-    --CHR ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "X", "Y"] \
-
-    --files 'data/seqs/SRR*_{1,2}.fastq' \
-    --referenceGenome 'data/genome/referenceGenome.fa' \
-    --annotatedGenome 'data/annotation/annotedGenome.gtf' \
-    --GenomeDir 'data/index/GenomeDir/' \
-    --trimmoFiles 'data/trimmomatic/*{1,2}P.fastq' \
-    --mappingFiles 'data/map/*bam' \
-    --baiFiles 'data/map/*bai'
+    --differentialAnalysis true
